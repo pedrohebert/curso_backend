82e5692c9a26c481b668fb0969b4b41ef6e4863b
pedrohebert <pedrohebert06@gmail.com>
2026-05-06 03:58:52 -0300

    feat(sprint3): api basica, modelo de task

--- sprint3/app/main.py
+++ sprint3/app/main.py
@@ -1,13 +1,65 @@
 from fastapi import FastAPI
+from typing import Sequence, TypedDict
+
+from fastapi.exceptions import HTTPException
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Task(TypedDict):
+    title: str
+    description: str
+    status: str
+
+class PublicTask(Task):
+    id:int
+
+class UpdateTask(TypedDict, total=False):
+    title: str | None
+    description: str | None
+    status: str | None
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+db:dict[int, Task] = {}
+
+
+def id_gen():
+    id = 0
+    while True:
+        yield id
+        id += 1
+
+
+_id = id_gen()
+
+
+def get_new_id() -> int:
+    return next(_id)
+
+
+@app.post("/")
+async def create_task(task: Task) -> PublicTask:
+    id = get_new_id()
+    db[id] = task
+    return {**task, "id":id}
+
+@app.get("/all")
+async def get_all_task() -> list[PublicTask]:
+    saida:list[PublicTask] = [{**v, "id":k} for k,v in db.items()]
+    return saida
+
+@app.get("/{task_id}")
+async def get_by_id(task_id:int) -> PublicTask:
+    if task_id not in db:
+        raise HTTPException(status_code=404, detail="task not fould")
+    return {**db[task_id], "id":task_id}
+
+@app.put("/")
+async def update_task(id:int,update: UpdateTask) -> PublicTask:
+    if id not in db:
+        raise HTTPException(status_code=404, detail="task not fould")
+
+    for k,v in db[id].items():
+        db[id][k] = update.get(k) if update.get(k) is not None else v
+
+    return {**db[id], "id":id}
