f16ff2c956e0bb8aab1a0870a753d2ffdf672126
pedrohebert <pedrohebert06@gmail.com>
2026-05-07 01:53:08 -0300

    style(sprint3): auto format

--- sprint3/app/main.py
+++ sprint3/app/main.py
@@ -6,9 +6,10 @@ from fastapi.exceptions import HTTPException
 
 app = FastAPI()
 
+
 class Status(Enum):
     TO_DO = "TO DO"
-    PROGRESS =  "IN PROGRESS"
+    PROGRESS = "IN PROGRESS"
     COMPLETED = "COMPLETED"
 
 
@@ -17,8 +18,10 @@ class Task(TypedDict):
     description: str
     status: Status
 
+
 class PublicTask(Task):
-    id:int
+    id: int
+
 
 class UpdateTask(TypedDict, total=False):
     title: str | None
@@ -26,7 +29,7 @@ class UpdateTask(TypedDict, total=False):
     status: Status | None
 
 
-db:dict[int, Task] = {}
+db: dict[int, Task] = {}
 
 
 def id_gen():
@@ -47,31 +50,35 @@ def get_new_id() -> int:
 async def create_task(task: Task) -> PublicTask:
     id = get_new_id()
     db[id] = task
-    return {**task, "id":id}
+    return {**task, "id": id}
+
 
 @app.get("/all")
 async def get_all_task() -> list[PublicTask]:
-    saida:list[PublicTask] = [{**v, "id":k} for k,v in db.items()]
+    saida: list[PublicTask] = [{**v, "id": k} for k, v in db.items()]
     return saida
 
+
 @app.get("/{task_id}")
-async def get_by_id(task_id:int) -> PublicTask:
+async def get_by_id(task_id: int) -> PublicTask:
     if task_id not in db:
         raise HTTPException(status_code=404, detail="task not fould")
-    return {**db[task_id], "id":task_id}
+    return {**db[task_id], "id": task_id}
+
 
 @app.patch("/")
-async def update_task(id:int,update: UpdateTask) -> PublicTask:
+async def update_task(id: int, update: UpdateTask) -> PublicTask:
     if id not in db:
         raise HTTPException(status_code=404, detail="task not fould")
 
-    up = list((k,v) for k,v in update.items() if v is not None)
+    up = list((k, v) for k, v in update.items() if v is not None)
     db[id].update(up)
 
-    return {**db[id], "id":id}
+    return {**db[id], "id": id}
+
 
 @app.delete("/{task_id}")
-async def delete_task(task_id:int) -> PublicTask:
+async def delete_task(task_id: int) -> PublicTask:
     if task_id not in db:
         raise HTTPException(status_code=404, detail="task not fould")
 
