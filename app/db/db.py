f075a9a1a54bf43759405c281d9bb9c939bbedb7
pedrohebert <pedrohebert06@gmail.com>
2026-05-19 18:34:27 -0300

    feat: função de busca filtrada no db

--- app/db/db.py
+++ app/db/db.py
@@ -15,7 +15,7 @@ class DB:
     def get_new_id(self) -> int:
         return next(self.__id)
 
-    def create(self, new_task: Task,/) -> int:
+    def create(self, new_task: Task, /) -> int:
         new_id = self.get_new_id()
         self.__db[new_id] = new_task
         return new_id
@@ -23,10 +23,31 @@ class DB:
     def get_all(self) -> dict[int, Task]:
         return self.__db
 
-    def get(self, task_id: int,/) -> Task | None:
+    def get(self, task_id: int, /) -> Task | None:
         return self.__db.get(task_id)
 
-    def delete(self, task_id: int,/) -> Task | None:
+    def get_filtred(
+        self,
+        title: str | None = None,
+        description: str | None = None,
+        status: str | None = None,
+    ):
+        return {
+            id: task
+            for id, task in self.get_all().items()
+            if (
+                (title is None or title in task.title)
+                and (
+                    description is None
+                    or (
+                        task.description is not None and description in task.description
+                    )
+                )
+                and (status is None or status in task.status.value)
+            )
+        }
+
+    def delete(self, task_id: int, /) -> Task | None:
         return self.__db.pop(task_id)
 
     def update(self, task_id: int, update_task: UpdateTask, /) -> Task | None:
